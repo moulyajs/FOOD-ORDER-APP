bd22cc292a5e594b751d6b4b275cfb123791d5a9
Moulya <moulya.anjanappa@gmail.com>
2024-08-17 19:02:01 +0530

    updated

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,25 @@
+import { useContext } from 'react';
 import logoImg from '../assets/logo.jpg';
+import CartContext from '../store/CartContext';
+import UserProgressContext from '../store/UserProgressContext';
 export default function Header() {
+    const cartCtx = useContext(CartContext);
+    const userProgressCtx = useContext(UserProgressContext);
+    const totalCartItems = cartCtx.items.reduce((totalNumberOfItems,item) => {
+        return totalNumberOfItems + item.quantity;
+    },0);
+    function handleShowCart () {
+        userProgressCtx.showCart();
+    }
     return (
+
         <header id="main-header">
             <div id="title">
                 <img src={logoImg} alt="A restaurant"/>
                 <h1>ReactFood</h1>
             </div>
             <nav>
-                <button>Cart (0)</button>
+                <button onClick={handleShowCart}>Cart ({totalCartItems})</button>
             </nav>
         </header>
     );
--- src/store/CartContext.jsx
+++ src/store/CartContext.jsx
@@ -25,13 +25,43 @@ function CartReducer (state,action) {
         return {...state,items:updatedItems};
     }
     if (action.type === 'REMOVE_ITEM') {
-        
+        const existingCartItemIndex = state.items.findIndex(
+            (item) => item.id === action.id);
+        const existingCartItem = state.items[existingCartItemIndex];
+        const updatedItems = [...state.items];
+
+        if(existingCartItem.quantity === 1) {
+            updatedItems.splice(existingCartItemIndex,1);
+        } 
+        else{
+            const updatedItem = {
+                ...existingCartItem,
+                quantity : existingCartItem.quantity - 1,
+            };
+            updatedItems[existingCartItemIndex] = updatedItem;
+        }
+        return {...state,items:updatedItems};
     }
+
     return state;
 
 }
 export  function CartContextProvider ({children}) {
-    useReducer(CartReducer,{items:[]});
-    return <CartContext.Provider>{children}</CartContext.Provider>
+    const [cart, dispatchCartAction] = useReducer(CartReducer,{items:[]});
+    
+    function addItem(item){
+        dispatchCartAction({type:'ADD_ITEM',item});
+    }
+    function removeItem(id) {
+        dispatchCartAction({type:'REMOVE_ITEM',id});
+    }
+    const cartContext = {
+        items:cart.items,
+        addItem,
+        removeItem
+    };
+
+    console.log(cartContext);
+    return <CartContext.Provider value={cartContext}>{children}</CartContext.Provider>
 }
 export default CartContext;
